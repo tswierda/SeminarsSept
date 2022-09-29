6b2a320e3f67d29150275a458aea7a0bfcff5455
Christos Perchanidis <36992459+pmchrist@users.noreply.github.com>
2022-09-29 17:28:29 +0300

    Added documentation

--- assignment_2c.py
+++ assignment_2c.py
@@ -5,7 +5,24 @@ documentation and see if he can work with it.
 No cheating! Don't show or tell hem the code directly
 """
 def function_2c(w, x, y, z):
-
+    """
+    Function that does a bunch of mathematical operations with the input parameters and returns the results
+    Parameters
+    ----------
+    w : float
+        A random number
+    x : float
+        A random number
+    y : float
+        A random number
+    z : float
+        A random number
+    
+    Returns
+    -------
+    results : dict
+        A dictionary with the results
+    """
     multiplication = x * y
     division = x / y
     addition = w + z
