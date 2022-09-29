4e3185e42a970a7017bb6f12538c9d81ef018100
Christos Perchanidis <36992459+pmchrist@users.noreply.github.com>
2022-09-29 17:26:23 +0300

    Created Uppers_Lowers

--- assignment_1.py
+++ assignment_1.py
@@ -9,17 +9,11 @@ def lower_case(string):
     """
     Argument:
     string -- text you want to turn into lower case
-
     Returns:
     lower_string -- lower case version of string
-
     """
 
-    ### your code starts here
-
-    ### your code ends here
-
-    return lower_string
+    return  
 
 
 """
@@ -31,14 +25,9 @@ def upper_case(string):
     """
     Argument:
     string -- text you want to turn into upper case
-
     Returns:
     upper_string -- upper case version of string
     
     """
 
-    ### your code starts here
-
-    ### your code ends here
-
-    return upper_string
+    return string.lower()
