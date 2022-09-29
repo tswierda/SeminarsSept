4bbb96bba5ef1854a02d692bbdc9915c04306de8
jop611 <meijer.jop@gmail.com>
2022-09-29 21:47:19 +0200

    Documentation added to function

--- assignment_2c.py
+++ assignment_2c.py
@@ -5,6 +5,20 @@ documentation and see if he can work with it.
 No cheating! Don't show or tell hem the code directly
 """
 def function_2c(w, x, y, z):
+    """
+    Arguments: 
+    w, x, y, z; numerical values (int or float).
+
+    Performs the following:
+    - multiplication of x and y
+    - division of x by y 
+    - addition of z to w
+    - subtracts z from w
+
+    Returns: 
+    dictionary with keys "multiply", "divide", "add", and "subtract", with as values
+    the result of the corresponding performed mathematical operations 
+    """
 
     multiplication = x * y
     division = x / y
