92d631b4e1d318abb50f6209f5e3c310bcb04deb
Gerard <XxGerardxX@github.com>
2022-09-29 16:37:57 +0200

    klaar :)

--- assignment_2a.py
+++ assignment_2a.py
@@ -10,9 +10,9 @@ Run the script to see if you succeeded! PS: Multiple combinations are possible,
 just give a correct one.
 """
 
-var_1 = function_2c(...)
+var_1 = function_2c(-50,10,5,1000)["add"]
 
-var_2 = function_2b(...)
+var_2 = function_2b("Seminars", "Borrel")["C"]
 
 if var_1 == 950:
     print("Good job!")
