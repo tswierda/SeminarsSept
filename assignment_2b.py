6f4885db899aad37396cc304d916807f528c4468
Stregaw <lottecarotte@gmail.com>
2022-09-29 17:02:53 +0200

    2

--- assignment_2b.py
+++ assignment_2b.py
@@ -1,8 +1,9 @@
 """
-Write a documentation for the simple function below. Your partner will have to
-implement the function, without knowing the code. Send your partner the
-documentation and see if he can work with it.
-No cheating! Don't show or tell hem the code directly
+input = string1, string2
+changes string 1 into lowercase
+changes string2 into uppercase
+creates a dict with L: string1, U: string2, C: strings combined
+return dict
 """
 def function_2b(string_1, string_2):
 
