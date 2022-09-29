c85a507f1c48942a75c961523528408ff2793561
Gerard <95470260+XxGerardxX@users.noreply.github.com>
2022-09-29 16:06:43 +0200

    2b added

--- assignment_2b.py
+++ assignment_2b.py
@@ -5,7 +5,8 @@ documentation and see if he can work with it.
 No cheating! Don't show or tell hem the code directly
 """
 def function_2b(string_1, string_2):
-
+    """the function input is 2 strings that are combined and added to a dictionary. The output gives
+     a key value pair where the first key gives the first word, the second key the second word and the third key the two combined words"""
     lower = string_1.lower()
     upper = string_2.upper()
     combined = string_1 + string_2
@@ -15,3 +16,4 @@ def function_2b(string_1, string_2):
                "C": combined}
 
     return dict
+print(function_2b("aap","noot"))
