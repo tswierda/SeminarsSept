3d93ff0e117041440d8f3629301f5900e5f2f113
Thomas Wierda <2621446@student.vu.nl>
2022-09-29 14:07:52 +0200

    lower

--- assignment_1.py
+++ assignment_1.py
@@ -15,9 +15,7 @@ def lower_case(string):
 
     """
 
-    ### your code starts here
-
-    ### your code ends here
+    lower_string = string.lower()
 
     return lower_string
 
@@ -38,7 +36,7 @@ def upper_case(string):
     """
 
     ### your code starts here
-
+    
     ### your code ends here
 
-    return lower_string
+    return upper_string
