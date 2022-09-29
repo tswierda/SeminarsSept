f2f1c7f4e9584d308aee9911e9964535bc1eb887
agolombek <91390904+agolombek@users.noreply.github.com>
2022-09-29 15:53:16 +0200

    finished project

--- assignment_2b.py
+++ assignment_2b.py
@@ -4,6 +4,8 @@ implement the function, without knowing the code. Send your partner the
 documentation and see if he can work with it.
 No cheating! Don't show or tell hem the code directly
 """
+
+
 def function_2b(string_1, string_2):
 
     lower = string_1.lower()
