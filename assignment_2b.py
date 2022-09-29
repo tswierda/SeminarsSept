2306b6ad1e6d64774fdbaf04df1b731c6961d77e
Stregaw <lottecarotte@gmail.com>
2022-09-29 17:09:30 +0200

    hey

--- assignment_2b.py
+++ assignment_2b.py
@@ -1,12 +1,12 @@
-"""
-input = string1, string2
-changes string 1 into lowercase
-changes string2 into uppercase
-creates a dict with L: string1, U: string2, C: strings combined
-return dict
-"""
-def function_2b(string_1, string_2):
 
+def function_2b(string_1, string_2):
+    """
+    input = string1, string2
+    changes string 1 into lowercase
+    changes string2 into uppercase
+    creates a dict with L: string1, U: string2, C: strings combined
+    return dict
+    """
     lower = string_1.lower()
     upper = string_2.upper()
     combined = string_1 + string_2
--- assignment_2c.py
+++ assignment_2c.py
@@ -1,14 +1,14 @@
-"""
-input: w, x, y, z
-muliplies x and y
-divides x/y
-adds w+z
-substracts w-z
-stores results in a dict
-output: dict
-"""
-def function_2c(w, x, y, z):
 
+def function_2c(w, x, y, z):
+    """
+    input: w, x, y, z
+    muliplies x and y
+    divides x/y
+    adds w+z
+    substracts w-z
+    stores results in a dict
+    output: dict
+    """
     multiplication = x * y
     division = x / y
     addition = w + z
