bee9ff7c3430db748c9b37135dfa0a301242c70d
floorclarisse <floor.clarisse@gmail.com>
2022-09-29 14:13:36 +0200

    2b and 2c

--- assignment_2b.py
+++ assignment_2b.py
@@ -5,6 +5,16 @@ documentation and see if he can work with it.
 No cheating! Don't show or tell hem the code directly
 """
 def function_2b(string_1, string_2):
+    """
+    Convert a string to lower and upper case and combines the two strings
+    Arguments:
+        string_1    
+        string_2
+    
+    Returns:
+        dictionary containing keys, value pairs (L, string_1 in lower case), (U, string_2 in upper case)
+        (C, string_1 plus string_2)
+    """
 
     lower = string_1.lower()
     upper = string_2.upper()
