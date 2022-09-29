685729958956bd94b45c9a5fea68d3a1b069e39f
arendgeerlofs <arendgeerlofs@hotmail.nl>
2022-09-29 14:10:17 +0200

    Assignment 1 + 2b + 2c

--- assignment_2b.py
+++ assignment_2b.py
@@ -4,7 +4,18 @@ implement the function, without knowing the code. Send your partner the
 documentation and see if he can work with it.
 No cheating! Don't show or tell hem the code directly
 """
-def function_2b(string_1, string_2):
+def function_2b(string_1, string_2) ->dict:
+    """
+    Use 2 inputs strings to create dictionary containing 1 lower case string
+    1 upper case string and the combined strings
+    input:
+    string_1 : type = String
+    string_2 : type = String
+    output:
+    dict = dictionary containing first string as lower case
+                                 second string as upper case
+                                 combined strings as single string
+    """
 
     lower = string_1.lower()
     upper = string_2.upper()
@@ -15,3 +26,4 @@ def function_2b(string_1, string_2):
                "C": combined}
 
     return dict
+    
--- assignment_2c.py
+++ assignment_2c.py
@@ -4,7 +4,21 @@ implement the function, without knowing the code. Send your partner the
 documentation and see if he can work with it.
 No cheating! Don't show or tell hem the code directly
 """
-def function_2c(w, x, y, z):
+def function_2c(w, x, y, z) -> dict:
+    """
+    Returns four different results of mathematical
+    operations on the give inputs
+
+    Inputs:
+    w = number
+    x = number
+    y = number
+    z = number
+
+    Output:
+    results = dictionary of 4 different mathematical
+              combinations of the inputs
+    """
 
     multiplication = x * y
     division = x / y
