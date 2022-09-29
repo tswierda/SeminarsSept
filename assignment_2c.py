7ca4edc66f444982618332e64056f9587c97cb28
Anne Claus <aceclaus@MacBook-Pro-van-Anne.local>
2022-09-29 16:20:33 +0200

    2c

--- assignment_2c.py
+++ assignment_2c.py
@@ -5,6 +5,24 @@ documentation and see if he can work with it.
 No cheating! Don't show or tell hem the code directly
 """
 def function_2c(w, x, y, z):
+    """Gets and prints the spreadsheet's header columns
+
+    Parameters
+    ----------
+    w : float
+        variable used in addition (x+y) and subtraction (x-y)
+    x : float
+        variable used in multiplication x*y and division x/y
+    y : float
+        variable used in multiplication x*y and division x/y
+    z : float
+        variable used in addition (x+y) and subtraction (x-y)
+
+    Returns
+    -------
+    dict
+        Diconary with keys "multiply", "divide", "add", "subtract" 
+    """
 
     multiplication = x * y
     division = x / y
