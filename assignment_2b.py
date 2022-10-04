a28261ee94e95176d6c295c864201c87d4b2b80c
静 <107680827+jingac@users.noreply.github.com>
2022-10-04 19:22:31 +0200

    Update assignment_2b.py

--- assignment_2b.py
+++ assignment_2b.py
@@ -4,6 +4,12 @@ implement the function, without knowing the code. Send your partner the
 documentation and see if he can work with it.
 No cheating! Don't show or tell hem the code directly
 """
+
+#function_2b(string_1, string_2)
+#returns a dictionary('L': all lowercase letters of string_1,
+#'U': all uppercase letters of string_2,
+#'C': string_1 + string_2)
+
 def function_2b(string_1, string_2):
 
     lower = string_1.lower()
