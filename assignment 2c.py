ab4547bba2a7151fe84dfb46f213cdac833f4adb
Seafoodpot <114532261+Seafoodpot@users.noreply.github.com>
2022-10-04 19:48:59 +0200

    Update assignment 2c.py

--- assignment 2c.py
+++ assignment 2c.py
@@ -7,16 +7,3 @@ For addition, use the first + fourth variables.
 For subtraction, use the first - fourth variables.
 Then, put the results in a dictionary.
 """
-def function_2c(w, x, y, z):
-
-    multiplication = x * y
-    division = x / y
-    addition = w + z
-    subtraction = w - z
-
-    results = {"multiply": multiplication,
-               "divide": division,
-               "add": addition,
-               "subtract": subtraction}
-
-    return results
