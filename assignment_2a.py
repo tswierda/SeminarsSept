d89bc609dc517ecf7531d3080b1df1a44b10b296
magdakrv <113594035+magdakrv@users.noreply.github.com>
2022-10-05 17:39:03 +0200

    Add files via upload

--- assignment_2a.py
+++ assignment_2a.py
@@ -10,13 +10,15 @@ Run the script to see if you succeeded! PS: Multiple combinations are possible,
 just give a correct one.
 """
 
-var_1 = function_2c(...)
+var_1 = function_2c(1000, 5, 10, -50)
+var_1 = var_1['add']
 
-var_2 = function_2b(...)
+var_2 = function_2b('Seminars', 'Borrel')
+var_2 = var_2['C']
 
 if var_1 == 950:
     print("Good job!")
-
+#
 if var_2 == "SeminarsBorrel":
     print("Well done!")
 
