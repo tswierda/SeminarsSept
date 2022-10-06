47f0b8b6ff894148337e201512d00a2601c42f32
Panaceametis <juditshamon@gmail.com>
2022-10-06 22:51:25 +0200

    .

--- assignment_1.py
+++ assignment_1.py
@@ -15,9 +15,7 @@ def lower_case(string):
 
     """
 
-    ### your code starts here
-
-    ### your code ends here
+    lower_string = string.lower()
 
     return lower_string
 
@@ -37,8 +35,6 @@ def upper_case(string):
     
     """
 
-    ### your code starts here
-
-    ### your code ends here
+    upper_string = string.upper()
 
     return upper_string
